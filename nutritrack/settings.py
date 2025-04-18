53c76b18aaef26df7032d69c837e2bd3482f2544
Your Name <user@example.com>
2025-04-18 13:41:41 +0530

    Fix missing static files by replacing image references with Font Awesome icons

--- nutritrack/settings.py
+++ nutritrack/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-jdw%l@=n-c6z*prl3ym1i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,nutrition-tracker-lkzl.onrender.com').split(',')
 
 
 # Application definition
